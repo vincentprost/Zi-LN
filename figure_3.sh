cb9c48e394f5337d0cb7422a979156d9aaa526fb
vincentprost <vincent.prost1@gmail.com>
2021-03-21 19:13:26 +0100

    Update figure_3.sh

--- figure_3.sh
+++ figure_3.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+for seed {1..10};
 for n in 100; 
 do for zeros in 0 0.1 0.5 0.7 0.9;
 do for model in norta PZiLN; 
 do for method in Spiec-Easi-glasso Spiec-Easi-mb glasso mb magma-glasso magma-mb ZiLN-glasso ZiLN-mb sparcc flashweave; 
-do Rscript script_command_line.R -d 300 -t erdos_renyi -g $model -z $zeros -y 0.98 -n 100 -m $method -s {} -o zeros_ziln_{}.txt; 
-done; done; done; done
+do Rscript script_command_line.R -d 300 -t erdos_renyi -g $model -z $zeros -y 0.98 -n 100 -m $method -s $seed -o zeros_ziln_$seed.txt; 
+done; done; done; done; done
 
 cat zeros_ziln_*.txt > zeros_ziln.txt
 rm zeros_ziln_*.txt
