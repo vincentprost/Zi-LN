73f30a2bea90ad051787b514b4fef8a8e80d614c
vincentprost <vincent.prost1@gmail.com>
2021-03-21 19:14:19 +0100

    Update figure_6.sh

--- figure_6.sh
+++ figure_6.sh
@@ -5,7 +5,7 @@ for d in 100 300 500;
 do for topo in band erdos_renyi scale_free; 
 do for n in 50 100 300 1000; 
 do for method in Spiec-Easi-glasso Spiec-Easi-mb glasso mb magma-glasso magma-mb ZiLN-glasso ZiLN-mb sparcc flashweave; 
-do Rscript script_command_line.R -d $d -t $topo -n $n -m $method -s {} -o norta_{}.txt -g norta; 
+do Rscript script_command_line.R -d $d -t $topo -n $n -m $method -s $seed -o norta_$seed.txt -g norta; 
 done; done; done; done; done
 
 cat norta_*.txt > norta.txt
