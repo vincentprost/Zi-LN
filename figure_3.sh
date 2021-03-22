d5fc7579b1dd316323c7a933580e116e9f0ce448
vincentprost <vincent.prost1@gmail.com>
2021-03-22 17:35:19 +0100

    Update figure_3.sh

--- figure_3.sh
+++ figure_3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for seed {1..10};
-for n in 100; 
+do for n in 100; 
 do for zeros in 0 0.1 0.5 0.7 0.9;
 do for model in norta PZiLN; 
 do for method in Spiec-Easi-glasso Spiec-Easi-mb glasso mb magma-glasso magma-mb ZiLN-glasso ZiLN-mb sparcc flashweave; 
