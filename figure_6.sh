33bf81e9903dc81cb9b5f64cec1212299f2e1cbc
vincentprost <vincent.prost1@gmail.com>
2021-03-22 17:35:35 +0100

    Update figure_6.sh

--- figure_6.sh
+++ figure_6.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for seed {1..10};
-for d in 100 300 500; 
+do for d in 100 300 500; 
 do for topo in band erdos_renyi scale_free; 
 do for n in 50 100 300 1000; 
 do for method in Spiec-Easi-glasso Spiec-Easi-mb glasso mb magma-glasso magma-mb ZiLN-glasso ZiLN-mb sparcc flashweave; 
